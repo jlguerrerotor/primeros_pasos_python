58dbf13942761449e31e5b8b146e112e80e9d728
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-02 18:05:19 +0200

    Create 01_variables.py

---
+++ 01_variables.py
@@ -0,0 +1,16 @@
+my_string_variable = "Esto es un string"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+print(type(my_int_variable))
+
+my_int_to_string_variable = str(my_int_variable)
+print(my_int_to_string_variable)
+print(type(my_int_to_string_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+print(type(my_bool_variable))
+
+print(my_string_variable, my_int_variable, my_int_to_string_variable, my_bool_variable) # Concatena variables aunque sean de tipos distintos
