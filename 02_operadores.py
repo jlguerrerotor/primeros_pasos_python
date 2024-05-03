346688569fa035d6d64234650677a7195da440c4
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-03 17:40:32 +0200

    Create 02_operadores.py

---
+++ 02_operadores.py
@@ -0,0 +1,33 @@
+### Operadores Aritméticos ###
+
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+print(10 / 3)
+print(10 // 3) # División entera
+print(10 % 3) # Módulo (resto de la división)
+print(2 ** 3) # Exponente 2 elevado a 3
+
+print("Hola" + "Python") # Concatena 2 cadenas de texto
+print("Hola" * 5) # Concatena 5 veces la palabra Hola. HolaHolaHolaHolaHola
+
+### Operadores Comparativos ###
+
+print(2 > 4)
+print(2 < 4)
+print(2 >= 4)
+print(2 <= 4)
+print(2 == 4)
+print(2 != 4)
+
+print("Hola" > "Python") # Ordenación alfabética por ASCII
+
+### Operadores Lógicos ###
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" < "Python")
+print(3 < 4 or ("Hola" < "Python" and 4  == 4))
+
+print(not(3 > 4))
