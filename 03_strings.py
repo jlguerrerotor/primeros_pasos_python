10f48b1a333ea76744afbd130caa369fe750acac
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-05 18:53:47 +0200

    04_listas

--- 03_strings.py
+++ 03_strings.py
@@ -23,3 +23,34 @@ name, surname, edad = "Joan Lluís", "Guerrero", 50
 
 print("Mi nombre es {} {} y mi edad es {}".format(name, surname, edad))
 print("Mi nombre es %s %s y mi edad es %d" %(name, surname, edad))
+print(f"Mi nombre es {name} {surname} y mi edad es {edad}".format(name, surname, edad))
+
+# Desempaquetado de caracteres
+language = "Python"
+a, b, c, d, e, f = language
+print(a)
+print(e)
+
+# División
+laguage_slice = language[1:3] # de 1 a 2. Comienza por 0, 1 y menor que 3
+print(laguage_slice)
+laguage_slice = language[1:] # de 1 a 2. Comienza por 0, 1 y el resto
+print(laguage_slice)
+laguage_slice = language[-2] # segundo caracter desde el final
+print(laguage_slice)
+
+laguage_slice = language[0:6:2] # Pilla los caracteres del 0 al 6 dando saltos de 2 en 2
+print(laguage_slice)
+
+# Reverse
+reversed_language = language[::-1]
+print(reversed_language)
+
+#Funciones
+print(language.capitalize()) # POne la primera letra en mayúscula
+print(language.upper()) # Pone todo en mayúscula
+print(language.count("t")) # Cuenta cuantas veces aparece la cadena en el string
+print(language.isnumeric()) # Indica si la cadena es un numérico
+print(language.lower()) # Pone todo en minúsculas
+print(language.upper().isupper()) # Indica se la cadena es mayúscula o no
+print(language.startswith("Py")) # Indica si la variable empieza por la cadena
---
+++ 04_listas.py
@@ -0,0 +1,55 @@
+### Listas ###
+
+# Creación
+my_list = list()
+mu_other_list = []
+
+print(len(my_list))
+
+# Inicialización / Relleno
+my_list = [35, 50, 24, 62, 53, 30, 17]
+
+print(my_list)
+print(len(my_list)) # Cuenta el número de elementos de la lista
+
+my_other_list = [50, 1.73, "Joan Lluís", "Guerrero Torta"]
+print(my_other_list)
+print(type(mu_other_list))
+
+# Acceso a la lista
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1]) # Accede al último elemento de la lista
+print(my_other_list[-3])
+print(my_other_list.count("Guerrero Torta")) # Count cuenta el número de ocurrencias de la cadena pasada en la lista
+# print(my_other_list[4])  # IndexError
+# print(my_other_list[-5]) # IndexError
+
+age, height, name, surname = my_other_list # Desempaqueta TODOS elementos en variables.
+print(name)
+
+print(my_list + my_other_list) # Concatenación de listas
+
+# Añadir elementos
+my_other_list.append("Mi Empresa") # Inserta al final de la lista
+print(my_other_list)
+
+my_other_list.insert(4,"Rojo") # Inserta un string en una posición de la lista
+print(my_other_list)
+
+my_other_list.remove("Rojo") # Elimina del la lista el primer elemento que coincie con el parámetro
+print(my_other_list)
+
+print(my_list.pop()) # Quita el último elemento de la lista y devuelve el valor que quita 
+print(my_list)
+
+my_pop_element = my_list.pop(2) # Quita el elemento de la lista indicado por el índice y devuelve el valor que quita 
+print(my_pop_element)
+print(my_list)
+
+del my_list[2] # Elimina un elemento de la lista 
+print(my_list)
+
+my_list.clear() # Vacía toda la lista
+print(my_list)
+
