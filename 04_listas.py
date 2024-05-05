ef8b4aa2c597baca721ed4448ca3a4b2bbe78319
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-05 19:07:03 +0200

    Update 04_listas.py

--- 04_listas.py
+++ 04_listas.py
@@ -40,6 +40,8 @@ print(my_other_list)
 my_other_list.remove("Rojo") # Elimina del la lista el primer elemento que coincie con el parámetro
 print(my_other_list)
 
+my_other_list.insert(4, "Rojo")
+
 print(my_list.pop()) # Quita el último elemento de la lista y devuelve el valor que quita 
 print(my_list)
 
@@ -51,5 +53,24 @@ del my_list[2] # Elimina un elemento de la lista
 print(my_list)
 
 my_list.clear() # Vacía toda la lista
+print(my_list)
+
+my_other_list.remove("Rojo")
+my_other_list.insert(4, "Azul") # Reemplazamos el Rojo por el Azul
+print(my_other_list)
+
+my_other_list[4] = "Rojo" # Reemplazamos el Azul por el Rojo de nuevo
+print(my_other_list)
+
+my_new_list = my_other_list.copy() # Hace una copia idéntica de la lista en otra posición de memoria
+my_other_list.clear() # Borra la lista original
+print(my_other_list) # Vacía
+print(my_new_list) # Copia
+
+my_new_list.reverse() # Cambia a orden inverso los valores de la lista
+print(my_new_list)
+
+my_list = [35, 50, 24, 62, 53, 30, 17]
+my_list.sort() # Ordena lis valores de la lista (No sirve para float ni str)
 print(my_list)
 
