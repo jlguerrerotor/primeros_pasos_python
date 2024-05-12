8991a156e71856349bc13565224dd2c2261f5c99
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-12 19:47:22 +0200

    Update 04_func_orden_superior.py

--- Intermediate/04_func_orden_superior.py
+++ Intermediate/04_func_orden_superior.py
@@ -7,8 +7,22 @@ def sum_one(value):
 def sum_five(value):
     return value + 5
 
-def sum_two_values_and_one(first_value, second_value, f_sum): # Estamos pasando un parámetro que en realidad es una función
-    return f_sum(first_value + second_value) # COmo resultado devuelve la suma de los dos valores y la aplica la función f_sum
+def sum_two_values_and_one(first_value, second_value, f_sum): # Estamos pasando un parámetro que en realidad es una función (f_sum)
+    return f_sum(first_value + second_value) # Como resultado devuelve la suma de los dos valores y la aplica la función f_sum
 
 print(sum_two_values_and_one(5, 2, sum_one))
 print(sum_two_values_and_one(5, 2, sum_five))
+
+### Closures ###
+
+def sum_ten(original_value):
+    def add(value): # Función devuielta
+        return value + original_value + 10
+    return add # Devuelve una función
+
+# Forma de llamar a sum_ten
+add_closure = sum_ten(1)
+print(add_closure(5))
+
+# Otra forma de llamar a sum_ten como una lambda
+print(sum_ten(5)(1)) # A sum_ten le pasa un 5 i al resultado (una función) le pasamos 1.
