6d81f19633fe01d835445f55611557ea23ab009c
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-13 18:49:52 +0200

    Update 04_func_orden_superior.py

--- Intermediate/04_func_orden_superior.py
+++ Intermediate/04_func_orden_superior.py
@@ -1,6 +1,9 @@
 ### Funciones de Orden Superior ###
 # Funciones que hacen cosas con funciones dentro. Parecido a funciones anidadas
 
+from functools import reduce
+
+
 def sum_one(value):
     return value + 1
 
@@ -26,3 +29,31 @@ print(add_closure(5))
 
 # Otra forma de llamar a sum_ten como una lambda
 print(sum_ten(5)(1)) # A sum_ten le pasa un 5 i al resultado (una función) le pasamos 1.
+
+### Build-in Funciones de Orden Superior ###
+
+numbers = [2, 5, 10, 21, 3, 30]
+
+# Map - Itera cada un o de los valores del elemento iterable (lista) y le aplica a cada uno la función pasada (multiply_two)
+
+def multiply_two(number):
+    return number * 2
+
+print(list(map(multiply_two, numbers)))
+print(list(map(lambda number: number*2, numbers)))
+
+# Filter
+
+def filter_greater_than_ten(numero):
+    return numero > 10
+
+print(list(filter(filter_greater_than_ten, numbers)))
+print(list(filter(lambda number: number > 10, numbers)))
+
+# Reduce - Itera la lista (iterable) acumulando el resultado de la función (suma) al siguiente valor
+# 2+5=7+10=17+21=38+3=41+30=71
+
+def sum_two_values(first_value, second_value):
+    return first_value + second_value
+
+print(reduce(sum_two_values, numbers))
