1ca03b4b2597617f273c7bda9147e86b0b3e8ea6
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-20 19:49:11 +0200

    arithmetics package

--- Intermediate/08_manejo_paquetes.py
+++ Intermediate/08_manejo_paquetes.py
@@ -12,6 +12,8 @@
 
 import numpy
 
+import mypackage.arithmetics
+
 print(numpy.version.version)
 
 numpy_array = numpy.array([35, 50, 24, 62, 53, 30, 17])
@@ -27,3 +29,9 @@ response = requests.get("https://pokeapi.co/api/v2/pokemon?limit=151") # Llamada
 print(response) # <Response [200]>
 print(response.status_code) # 200
 print(response.json()) # Es el json que contiene todos los datos solicitados en la llamada a la API
+
+# Arithmetics Package
+
+from mypackage import arithmetics
+
+print(arithmetics.sum_two_values(1, 4))
