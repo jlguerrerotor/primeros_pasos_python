4a6f1edb62fbf8d00a87632e89223dfbeba242a7
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-20 21:12:03 +0200

    Update main.py

--- backend/FastAPI/main.py
+++ backend/FastAPI/main.py
@@ -2,3 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/") # Vamos a acceder a la raíz de un sitio
+async def root(): # Siempre que accedemos a un servidor la operación debe ser asíncrona.
+    return "¡Hola FastAPI!"
