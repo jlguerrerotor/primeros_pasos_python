67c71eb3043aed34280733d339a17150f85c0e93
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-23 20:33:17 +0200

    Operaciones

--- backend/FastAPI/users.py
+++ backend/FastAPI/users.py
@@ -1,5 +1,5 @@
 # Arrancar el servidor mediante $ uvicorn users:app --reload
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel # Para poder definir objetos.
 
 app = FastAPI()
@@ -23,7 +23,7 @@ async def usersjson():
             {"name":"Andrea","surname":"Peris","url":"https://andre.com","age":20}]
 
 # GET de Users
-@app.get("/users")
+@app.get("/users/")
 async def users():
     return users_list
 
@@ -35,10 +35,54 @@ async def user(id: int):
 
 # GET - Parametros de Query
 # Se suele usar para parámetros que pueden NO ser obligatorios
-# Ejemplo devolver un nº determinado de usuarios cunado hay muchos
+# Ejemplo devolver un nº determinado de usuarios cuaddo hay muchos (Paginación)
 @app.get("/user/") # Parámetros a través de query http://127.0.0.1:8000/user/?id=2
 async def user(id: int):
     return search_user(id)
+
+# POST de User
+# Insertar datos
+# http://127.0.0.1:8000/user/ -> JSON {"id":4,"name":"Cinta","surname":"Peris","url":"https://moure.cat","age":45}
+# status_code indica el codigo de mensaje que queremos que devuelva por defecto si la operación sale bien. 201 -> Creado
+# response_molde indica (de cara a la documentación) el tipo de objeto que devuelve la función
+@app.post("/user/", response_model=User, status_code=201)
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        raise HTTPException(status_code=204,detail="El usuario ya existe") # Lanza la excepción indicada
+    else: # Este else no haria falta ponerlo ya que en el if hace un return
+        users_list.append(user)
+        return user
+
+# PUT de User
+# Modificar un dato (edad)
+# http://127.0.0.1:8000/user/ -> JSON {"id":4,"name":"Cinta","surname":"Peris","url":"https://moure.cat","age":18}
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error":"No se ha actualizado el usuario"}
+    else: # Este else no haria falta ponerlo ya que en el if hace un return
+        return user
+
+# DELETE de User
+# Eliminar un usuario
+# http://127.0.0.1:8000/user/4
+@app.delete("/user/{id}")
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    if not found:
+        return {"error":"No se ha eliminado el usuario"}
+    else: # Este else no haria falta ponerlo ya que en el if hace un return
+        return {"id": id}
+
 
 def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list) # Buscamos el usuario en la lista
