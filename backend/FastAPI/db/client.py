0e6293bd3b146dfe2bb3e796756c87e4cd2a73ee
jlguerrerotor <jlguerrerotor@users.noreply.github.com>
2024-05-28 19:36:35 +0200

    MongoDB Remoto

--- backend/FastAPI/db/client.py
+++ backend/FastAPI/db/client.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient()
+# Base de Datos Local
+# db_client = MongoClient().local
+
+# Base de Datos Remota
+db_client = MongoClient("mongodb+srv://jlguerrerotoria:dncrlM8itUAT0Bqx@cluster0.lvy5jil.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").test
--- backend/FastAPI/routers/users_db.py
+++ backend/FastAPI/routers/users_db.py
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/userdb",
 # GET de Users http://127.0.0.1:8000/userdb
 @router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(db_client.users.find())
 
 # GET - Parámetros de PATH
 # Se suele usar para parámetros que són obligatorios y fijos
@@ -41,9 +41,9 @@ async def user(user: User):
     user_dict = dict(user)
     del user_dict["id"]
 
-    id = db_client.local.users.insert_one(user_dict).inserted_id
+    id = db_client.users.insert_one(user_dict).inserted_id
 
-    new_user = user_schema(db_client.local.users.find_one({"_id": id}))
+    new_user = user_schema(db_client.users.find_one({"_id": id}))
 
     return User(**new_user)
 
@@ -57,7 +57,7 @@ async def user(user: User):
     del user_dict["id"]
     
     try:
-        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+        db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
     except:
         return {"error":"No se ha actualizado el usuario"}
     
@@ -68,14 +68,14 @@ async def user(user: User):
 # http://127.0.0.1:8000/userdb/6654ad6f9217890b78ad66b2
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def user(id: str):
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
 
     if not found:
         return {"error":"No se ha eliminado el usuario"}
 
 def search_user(field: str, key):
     try: 
-        user = db_client.local.users.find_one({field: key})
+        user = db_client.users.find_one({field: key})
         return User(**user_schema(user))
     except: # Si no ha encontrado ningún usuario devuelve un error en formato JSON
         return {"error":"No se ha encontrado el usuario"}
